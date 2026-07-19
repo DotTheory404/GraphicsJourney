f7b21c178ce2da2865b1a8b47b9e1307f0cffe21
s1n1s_01 <alexandrinaovchinnikova@gmail.com>
2026-07-19 15:07:09 +0300

    end 2nd weeks of studing

--- day14-15.cpp
+++ day14-15.cpp
@@ -6,7 +6,6 @@ typedef int PringError;
 namespace day14{
 
 	template <typename T>
-
 	T add(T a, T b) {
 		return a + b;
 	}
@@ -28,6 +27,16 @@ namespace day14{
 		return product;
 	}
 
+	template <typename T>
+	T mult(T x, int y) {
+		return x * y;
+	}
+
+	template <typename T, typename N>
+	auto sub(T x, N y) {
+		return x - y;
+	}
+		
 
 }
 
@@ -36,11 +45,21 @@ int main() {
 	//std::cout << day14::printData() << '\n';
 	//std::cout << day14::add<>(2, 4);
 
-	static_assert(day14::factorial<0>() == 1);
-	static_assert(day14::factorial<3>() == 6);
-	static_assert(day14::factorial<5>() == 120);
+	//static_assert(day14::factorial<0>() == 1);
+	//static_assert(day14::factorial<3>() == 6);
+	//static_assert(day14::factorial<5>() == 120);
 
 	//day14::factorial<-3>(); // should fail to compile
+
+	//std::cout << day14::add(2, 3) << '\n';
+	//std::cout << day14::add(1.2, 3.4) << '\n';
+
+	//std::cout << day14::mult(2, 3) << '\n';
+	//std::cout << day14::mult(1.2, 3) << '\n';
+
+	std::cout << day14::sub(3, 2) << '\n';
+	std::cout << day14::sub(3.5, 2) << '\n';
+	std::cout << day14::sub(4, 1.5) << '\n';
 
 	return 0;
 }
